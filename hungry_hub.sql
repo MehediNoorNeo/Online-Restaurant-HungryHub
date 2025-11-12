6694fd2293c925702e9980259ff1540225d27782
Mehedi Noor <91518232+MehediNoorNeo@users.noreply.github.com>
2025-11-13 01:05:05 +0600

    Add files via upload

---
+++ hungry_hub.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2025 at 08:02 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hungry_hub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+-- Error reading structure for table hungry_hub.admin_users: #1932 - Table &#039;hungry_hub.admin_users&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.admin_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`admin_users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `areas`
+--
+-- Error reading structure for table hungry_hub.areas: #1932 - Table &#039;hungry_hub.areas&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.areas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`areas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupons`
+--
+-- Error reading structure for table hungry_hub.coupons: #1932 - Table &#039;hungry_hub.coupons&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.coupons: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`coupons`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_items`
+--
+-- Error reading structure for table hungry_hub.food_items: #1932 - Table &#039;hungry_hub.food_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.food_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`food_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_menu`
+--
+-- Error reading structure for table hungry_hub.food_menu: #1932 - Table &#039;hungry_hub.food_menu&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.food_menu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`food_menu`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table hungry_hub.orders: #1932 - Table &#039;hungry_hub.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table hungry_hub.password_resets: #1932 - Table &#039;hungry_hub.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+-- Error reading structure for table hungry_hub.reviews: #1932 - Table &#039;hungry_hub.reviews&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.reviews: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`reviews`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table hungry_hub.users: #1932 - Table &#039;hungry_hub.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table hungry_hub.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `hungry_hub`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
